5ce47d70eaf2ff86e627765392c33bc3f5caa0d6
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-02-11 16:48:03 +0900

    Update 02-language.sh

--- Fedora/02-language.sh
+++ Fedora/02-language.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 pkg_list=(
+    fcitx5
     fcitx5-mozc
     fcitx5-autostart
 )
@@ -19,15 +20,5 @@ LOG_FILE="$(PARENT_DIR)/log/installation-_$(date +%Y%m%d-%H%M%S).txt"
 for pkg in "${pkg_list}"; do
     install_packages "$pkg" "$LOG_FILE"
 done
-
-echo "Installation is completed! Log is saved in $(LOG_FILE)."
-
-echo 'Setting up input method ...' && wait 1
-
-cat << EOF >> ~/.xprofile
-export XMODIFIERS=@im=fcitx
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-EOF
 
 echo '*** $HOME/.xprofile has been created.'
