e13f22c0476bb7fff7c1d2e5e0bf48382e72f56e
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-02-11 09:20:58 +0900

    Update install.sh

--- Debian/install.sh
+++ Debian/install.sh
@@ -3,12 +3,12 @@
 # パッケージのインストール処理を行う関数
 install_packages() {
     # パッケージがインストールされているか確認
-    if pacman -Qi "$1" &> /dev/null; then
+    if dpkg -l | grep -q -w "$1" &> /dev/null; then
         echo "$1 is already installed. Skipping."
     else
         echo "Installing $1 ..."
-        ( sudo pacman -S --noconfirm "$1" 2>&1) >> $2 2>&1 &
-        if pacman -Qi "$1" &> /dev/null; then
+        ( sudo apt install -y "$1" 2>&1) >> $2 2>&1 &
+        if dpkg -l | grep -q -w "$1" &> /dev/null; then
             echo "$1 is installed successfully."
         else
             echo "$1 is failed to be installed."
