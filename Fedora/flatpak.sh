8472e3c7fce865884ab371e45d59adbc6df17844
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-02-11 09:27:38 +0900

    Update flatpak.sh

--- Fedora/flatpak.sh
+++ Fedora/flatpak.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo 'Installing flatpak ...'
-sudo emerge -a flatpak
+sudo dnf install -y flatpak
 echo 'Adding flathub repository ...'
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
@@ -9,8 +9,8 @@ echo 'Installing warehouse ...'
 flatpak install -y flathub io.github.flattool.Warehouse
 
 # OBS Studio
-#echo 'Installing OBS Studio ...'
-#flatpak install -y flathub com.obsproject.Studio
+echo 'Installing OBS Studio ...'
+flatpak install -y flathub com.obsproject.Studio
 
 # GPU Screen Recorder
 echo 'Installing GPU Screen Recorder ...'
