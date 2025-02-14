65fa1a70c43c405022f1e0a0161f70491c3a1f16
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-02-14 18:22:09 +0900

    Update 02-language.sh

--- Arch/02-language.sh
+++ Arch/02-language.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+
+if [ -d log]; then
+    mkdir log
+fi
 
 pkgs=(
-	noto-fonts-cjk
+    noto-fonts-cjk
     noto-fonts-extra
     noto-fonts-emoji
     adobe-source-code-pro-fonts
@@ -10,21 +14,9 @@ pkgs=(
     fcitx5-mozc
 )
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Change the working directory to the parent directory of the script
-PARENT_DIR="$SCRIPT_DIR/.."
-cd "$PARENT_DIR" || exit 1
-
-source "$(dirname "$(readlink -f "$0")")/main.sh"
-
-# Set the name of the log file to include the current date and time
-LOG="Install-Logs/install-$(date +%d-%H%M%S)_bluetooth.log"
-
-# Install
 printf "Installing Packages...\n"
 for pkg in "${pkgs[@]}"; do
-    install_package "$pkg" "$LOG"
+    sudo pacman -S --needed --noconfirm $pkg 2>&1 || tee -a "log/log-language_$(date +%Y%m%d%H%M%S).txt
 done
 
 printf " Setting up fcitx5 ...\n"
